d8e56a3e12f3d7aa22e9c6883537414937430c21
NIDHINgl <abeynidhin.g.l@gmail.com>
2021-03-02 22:12:01 +0530

    notification sound

--- src/RemotePushController.js
+++ src/RemotePushController.js
@@ -4,8 +4,21 @@ import { DeviceEventEmitter } from "react-native";
 
 const RemotePushController = () => {
   useEffect(() => {
+    PushNotification.createChannel(
+      {
+        channelId: "638137827026",
+        channelName: "video noti",
+        channelDescription: "notification",
+        playSound: true,
+        soundName: "noti.mp3",
+        importance: 4,
+        vibrate: true,
+      },
+      (created) => console.log(`createChannel returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
+    );
     PushNotification.configure({
       // (optional) Called when Token is generated (iOS and Android)
+
       onRegister: function (token) {
         console.log("TOKEN:", token);
       }, // (required) Called when a remote or local notification is opened or received
@@ -24,10 +37,13 @@ const RemotePushController = () => {
           remoteMessage.notification
         );
       },
+
       // Android only: GCM or FCM Sender ID
       senderID: "638137827026",
       popInitialNotification: true,
       requestPermissions: true,
+      playSound: true,
+      soundName: "noti.mp3",
     });
   }, []);
 
